08f93945fd5f98564046a0e9bcbdbea3af63f2b0
pibouill <pibouill@student.42prague.com>
2023-10-29 16:28:01 +0100

    first ?

---
+++ inc/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pibouill <pibouill@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/29 13:52:47 by pibouill          #+#    #+#             */
+/*   Updated: 2023/10/29 14:18:21 by pibouill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
