df1b7ecf1909e36378331dcfc8a30ef5aae9ea17
pibouill <pibouill@student.42prague.com>
2023-10-29 16:45:32 +0100

    renaming

--- inc/libftprintf.h
+++ inc/libftprintf.h
@@ -1,22 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pibouill <pibouill@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 13:52:47 by pibouill          #+#    #+#             */
-/*   Updated: 2023/10/29 14:18:21 by pibouill         ###   ########.fr       */
+/*   Updated: 2023/10/29 16:43:36 by pibouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
+# include <libft.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
 
 #endif
